b2b01afff783a0e795090199f6b1e314738f3b18
ricardon-aguiar <ricardo.aguiar@estudantes.ifg.edu.br>
2026-06-08 21:25:41 -0300

    Aula - 2026-06-08

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+
+const app = express()
+app.use(express.json())
+
+const alunos = []
+
+app.get('/alunos', (pedido, resposta) => {
+    resposta.json(alunos)
+})
+
+app.post('/alunos', (pedido, resposta) => {
+    const aluno = {
+        matricula: pedido.body.matricula,
+        nome: pedido.body.nome,
+        data_nasc: pedido.body.data_nasc,
+        email: pedido.body.email
+    }
+
+    alunos.push(aluno)
+
+    resposta.json(aluno)
+})
+
+
+app.listen(3000, () => {
+    console.log('API rodando na porta 3000')
+})
